a6cfbc0383f786a3c5b77c12d068fccf6aa32ea0
James-Holloway <james.holloway@pyrites.org.uk>
2020-06-29 20:12:27 +0100

    Reduced Vignette Size to a minimum
    This is mostly learning how Harmony is used while reducing the vignette to a more appropriate level (very low)
    Also renamed project from initial commit to exclude the space inbetween

---
+++ Reduced Vingette/Startup.cs
@@ -0,0 +1,17 @@
+﻿using PulsarPluginLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReducedVingette
+{
+    public class ReducedVingette : PulsarPlugin
+    {
+        public override string HarmonyIdentifier()
+        {
+            return "uk.org.pyrites.james.ReducedVingette";
+        }
+    }
+}
---
+++ Reduced Vingette/VignetteOverride.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Reduced_Vingette
+{
+    [HarmonyPatch(typeof(PLPostProcessVignette), "")]
+    [HarmonyPatch("OnRenderImage")]
+    class VignetteOverride
+    {
+        static void Prefix(PLPostProcessVignette __instance, out float __state)
+        {
+            float multiplier = 0.5f;
+            __instance.VignetteAmount *= multiplier;
+            __instance.VignetteSize   *= multiplier;
+            __instance.ExposureBoost  *= multiplier;
+            __state = multiplier;
+        }
+        static void Postfix(PLPostProcessVignette __instance, float __state)
+        {
+            __instance.VignetteAmount *= 1 / __state;
+            __instance.VignetteSize   *= 1 / __state;
+            __instance.ExposureBoost  *= 1 / __state;
+        }
+    }
+}
